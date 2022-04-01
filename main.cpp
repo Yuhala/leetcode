a00249123d475e96ebe9baca2079b245cf6c25ca
Yuhala <petersonyuhala@gmail.com>
2022-04-01 08:43:55 +0200

    constant buffer size

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@
 void print_array(int *array, int size);
 
 // globals
-int array[5] = {5, 2, 3, 4, 1};
+#define BUF_SIZE 7
+int array[BUF_SIZE] = {1, 2, 3, 4, 7, 5, 6};
 
 int main()
 {
@@ -25,9 +26,9 @@ int main()
     printf("x = 0 output: %d\n", reverse_int(0));
     printf("x = 1534236469 output: %d\n", reverse_int(1534236469)); */
 
-    print_array(array, 5);
-    bubble_sort(array, 5);
-    print_array(array, 5);
+    print_array(array, BUF_SIZE);
+    bubble_sort(array, BUF_SIZE);
+    print_array(array, BUF_SIZE);
     return 0;
 }
 
